b0c140303749c16afceb0c9cda4eb039b4215bd7
Keago99 <keatabde@gmail.com>
2025-09-03 21:17:31 +0200

    Add tableReset method, learned what outerHTML is.

--- scripts.js
+++ scripts.js
@@ -13,6 +13,16 @@ function addBookToLibrary(title, author, pages, read){
     library.push(addedBook);
 }
 
+function updateTable(){
+    clearTable();
+}
+
+function clearTable(){
+    let table = document.getElementById("mainTable");
+    table.innerHTML = table.rows[0].outerHTML;
+}
+
 addBookToLibrary("Lord of the Rings", "Ringy dude", 300, "Yes");
 
 console.log(library);
+clearTable();
