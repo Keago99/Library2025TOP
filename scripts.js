24f6f5ed8ee76126100a1c84654ebb3e5000913d
Keago99 <keatabde@gmail.com>
2025-09-03 22:44:14 +0200

    update table working! Time for modal addition.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 const library = [];
 
+let tableBody = document.getElementById("tableBody");
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -15,14 +17,39 @@ function addBookToLibrary(title, author, pages, read){
 
 function updateTable(){
     clearTable();
+    
+
+    for (let i = 0; i < library.length; i++){
+        let row = document.createElement("tr");
+
+
+        let nTitle = document.createElement("td");
+        nTitle.innerHTML = library[i].title;
+        
+        let nAuthor = document.createElement("td");
+        nAuthor.innerHTML = library[i].author;
+
+        let nPages = document.createElement("td");
+        nPages.innerHTML = library[i].pages;
+
+        let nRead = document.createElement("td");
+        nRead.innerHTML = library[i].read;
+
+        row.appendChild(nTitle);
+        row.appendChild(nAuthor);
+        row.appendChild(nPages);
+        row.appendChild(nRead);
+        tableBody.appendChild(row);
+        console.log("done for 1");
+    }
+
 }
 
 function clearTable(){
-    let table = document.getElementById("mainTable");
-    table.innerHTML = table.rows[0].outerHTML;
+    tableBody.innerHTML = "";
 }
 
 addBookToLibrary("Lord of the Rings", "Ringy dude", 300, "Yes");
 
 console.log(library);
-clearTable();
+updateTable();
